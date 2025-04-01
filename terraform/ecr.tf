05d5f9fefdc00d205c15cef714ca9ef0ffb53535
NGrande-SAI <ngrande@slower.ai>
2025-04-01 11:37:19 -0400

    Readd ECR repos

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,15 +1,15 @@
-# locals {
-#   ecr_repository_names = ["consensus-genome", "diamond"]
-# }
+locals {
+  ecr_repository_names = ["consensus-genome", "diamond"]
+}
 
-# resource "aws_ecr_repository" "cypherid-consensus-genome" {
-#   for_each             = toset(local.ecr_repository_names)
-#   name                 = each.key
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "workflow-repositories" {
+  for_each             = toset(local.ecr_repository_names)
+  name                 = each.key
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
