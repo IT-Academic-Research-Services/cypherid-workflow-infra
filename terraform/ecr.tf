056b95b386b9c76fa129154cf96eabf5e6b15733
NGrande-SAI <ngrande@slower.ai>
2025-04-01 10:27:25 -0400

    Set up ECR repos to be created for each workflow type from list of strings

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,8 +1,15 @@
+locals {
+  ecr_repository_names = ["consensus-genome", "diamond"]
+}
+
 resource "aws_ecr_repository" "cypherid-consensus-genome" {
-  name                 = "consensus-genome"
+  for_each             = toset(local.ecr_repository_names)
+  name                 = each.key
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  tags = local.common_tags
 }
