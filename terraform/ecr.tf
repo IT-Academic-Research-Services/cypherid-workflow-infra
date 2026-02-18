5bd688ece2189826a7d8532ac22e163572644ac6
Joseph Sims <jsims@slower.ai>
2026-02-18 15:05:41 -0700

    Allow Terraform to delete ECR repos that contain images.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,6 +6,7 @@ resource "aws_ecr_repository" "workflow-repositories" {
   for_each             = toset(local.ecr_repository_names)
   name                 = each.key
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
