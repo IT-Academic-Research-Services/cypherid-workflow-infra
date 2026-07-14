5c817d008d6fd4747e873955538c0ecd30a3bf22
jsims-slower <jsims@slower.ai>
2026-07-14 17:07:58 -0600

    * Use swipe version v.1.5.1 from github.com/IT-Academic-Research-Services/swipe which includes bug fixes and deleting sample files and restricted intermediate files at the end of a Step Function
    * Update Step Functions to correctly reference changes in swipe

--- terraform/swipe.tf
+++ terraform/swipe.tf
@@ -2,7 +2,7 @@ module "swipe" {
   # TODO: Use chanzuckerberg/swipe once they merge in PR https://github.com/chanzuckerberg/swipe/pull/124
   #       But don't hold your breath. That repo hasn't been updated since April 2025
   # source = "github.com/chanzuckerberg/swipe?ref=v1.4.9"
-  source = "github.com/jsims-slower/swipe"
+  source = "github.com/IT-Academic-Research-Services/swipe?ref=v1.5.1"
   tags = {
     Name = "swipe"
   }
